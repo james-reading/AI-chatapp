b9a52b70f33d22abaca029ade336b2c1766aa364
James Reading <james.reading@immersivelabs.com>
2025-07-15 10:37:45 +0100

    Simple tool calling agent

--- backend/main.py
+++ backend/main.py
@@ -1,34 +1,24 @@
 import json
 from typing import Annotated
 from pydantic import BaseModel, Field
-from operator import add
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
 
 from langchain.chat_models import init_chat_model
-from langchain_core.messages import HumanMessage, AIMessageChunk
+from langchain_core.messages import HumanMessage, SystemMessage, ToolMessage, AIMessageChunk
+from langchain_core.tools import tool, InjectedToolCallId
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import START, END, MessagesState, StateGraph
-from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, InjectedState
+from langgraph.types import Command
 
-from langchain_core.callbacks import BaseCallbackHandler
-
-from requirements import graph as requirements_graph
-from briefing import graph as briefing_graph
-from questions import graph as questions_graph
 from config.settings import Settings
-from models import LabRequirements, Question
 
 class RequestParams(BaseModel):
     message: str
-
-class State(MessagesState):
-    step: str
-    requirements: LabRequirements
-    briefing: str
-    questions: Annotated[list[Question], add]
+    thread_id: str
 
 app = FastAPI()
 
@@ -41,42 +31,65 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
-def router(state: State):
-    return state.get("step", "requirements_graph")
+llm = init_chat_model("gpt-4.1", model_provider="openai", api_key=Settings().openai_api_key)
 
-workflow = StateGraph(State)
-workflow.add_node("requirements_graph", requirements_graph)
-workflow.add_node("briefing_graph", briefing_graph)
-workflow.add_node("questions_graph", questions_graph)
+class Question(BaseModel):
+    """A multiple choice question."""
+    title: str = Field(..., description="The question title")
+    options: list[str] = Field(..., description="The options for the question")
+    correct_option_index: int = Field(..., description="The index of the correct option, starting from 0")
 
-workflow.add_conditional_edges(START, router, ["requirements_graph", "briefing_graph", "questions_graph"])
+@tool
+def create_question(
+    question: str,
+    tool_call_id: Annotated[str, InjectedToolCallId]
+) -> "Command":
+    """Set the lab requirements."""
+    return Command(update={
+        "messages": [
+            ToolMessage(f"Question created successfully", tool_call_id=tool_call_id)
+        ]
+    })
+
+async def chat(state: MessagesState) -> MessagesState:
+    template = """If the user requests a question on a specific topic, create a question by calling the tool
+
+After calling the tool, DO NOT repeat the question, just ask if they would like another"""
+
+    messages = [SystemMessage(content=template)] + state["messages"]
+    llm_with_tool = llm.bind_tools([create_question])
+    response = await llm_with_tool.ainvoke(messages)
+    return {"messages": [response]}
+
+def should_continue(state):
+    messages = state["messages"]
+    last_message = messages[-1]
+    if last_message.tool_calls:
+        return "tools"
+    return END
+
+workflow = StateGraph(MessagesState)
+workflow.add_node("chat", chat)
+workflow.add_node("tools", ToolNode([create_question]))
+
+workflow.add_edge(START, "chat")
+workflow.add_conditional_edges("chat", should_continue, ["tools", END])
+workflow.add_edge("tools", "chat")
 
 memory = MemorySaver()
 
-async def stream_response(message: str):
+async def stream_response(request: RequestParams):
     graph = workflow.compile(checkpointer=memory)
-    config = {"configurable": {"thread_id": "1"}}
-    input_messages = [HumanMessage(message)]
+    config = {"configurable": {"thread_id": request.thread_id}}
+    input_messages = [HumanMessage(request.message)]
 
-    first = True
-    # Stream the workflow execution using messages mode
-    async for namespace, (chunk, metadata) in graph.astream({"messages": input_messages}, config, subgraphs=True, stream_mode="messages"):
-        print(chunk)
+    async for chunk, metadata in graph.astream({"messages": input_messages}, config, stream_mode="messages"):
         if isinstance(chunk, AIMessageChunk):
-            if chunk.tool_call_chunks:
-                if first:
-                    gathered = chunk
-                    is_first = False
-                else:
-                    gathered = gathered + chunk
-
-
-                yield f"data: {json.dumps({'type': 'tool', 'content': gathered.tool_calls})}\n\n"
-            else:
-                is_first = True # HACK: need a better way to isolate tool calls
-                yield f"data: {json.dumps({'type': 'thinking', 'content': chunk.content})}\n\n"
+            print(chunk.tool_call_chunks)
+            # Yield each chunk as a JSON object
+        yield f"{json.dumps({'type': 'stream', 'token': chunk.content})}\n"
 
 
 @app.post("/")
 async def chat(request: RequestParams):
-    return StreamingResponse(stream_response(request.message), media_type="text/event-stream")
+    return StreamingResponse(stream_response(request), media_type="text/event-stream")
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,11 @@
 import React, { useState, useRef } from 'react';
-import Markdown from 'react-markdown'
+import Markdown from 'react-markdown';
 
 function App() {
+  const threadId = useRef(); // Generate a unique thread ID
   const [messages, setMessages] = useState([]);
   const [inputMessage, setInputMessage] = useState('');
   const [streamedText, setStreamedText] = useState('');
-  const [requirements, setRequirements] = useState({
-    topic: '',
-    target_persona: '',
-    difficulty_level: '',
-  });
-  const [briefing, setBriefing] = useState('');
-  const [questions, setQuestions] = useState([
-
-  ]);
 
   async function chat(message) {
     const response = await fetch("http://127.0.0.1:8000", {
@@ -21,157 +13,76 @@ function App() {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ message }),
+      body: JSON.stringify({ message, thread_id: threadId.current.value })
     })
 
     const reader = response.body.pipeThrough(new TextDecoderStream()).getReader()
-    let thinkingText = '';
 
+    let finalText = '';
     while (true) {
       const { value, done } = await reader.read();
-      if (done) {
-        console.log('Stream finished');
-        break;
-      };
+      if (done) break;
 
-      value.split('\n').forEach((line) => {
-        if (line.trim() && line.startsWith('data: ')) {
-          const jsonData = line.substring(6); // Remove 'data: ' prefix
-          const parsed = JSON.parse(jsonData);
+      value.split('\n').forEach(line => {
+        line = line.trim();
+        if (!line) return;
 
-          // Thinking messages need to be streamed to the chat area
-          if (parsed.type === 'thinking') {
-            thinkingText += parsed.content;
-            setStreamedText(thinkingText);
-          } else if (parsed.type === 'tool') {
-            parsed.content.forEach((tool) => {
+        const data = JSON.parse(line);
 
-              switch (tool.name) {
-                case 'set_lab_requirements':
-                  setRequirements(tool.args.requirements || {});
-                  break;
-                case 'set_briefing':
-                  setBriefing(tool.args.briefing || "");
-                  break;
-                case 'create_question':
-                  setQuestions((prevQuestions) => {
-                    const existingQuestion = prevQuestions.find(q => q.id === tool.id);
-                    if (existingQuestion) {
-                      // Update existing question
-                      return prevQuestions.map(q => q.id === tool.id ? { ...q, ...tool.args.question } : q);
-                    } else {
-                      // Add new question
-                      return [...prevQuestions, { id: tool.id, ...tool.args.question }];
-                    }
-                  });
-                  break;
-              }
-            });
-          }
+        if (data.type === 'stream') {
+          finalText += data.token;
+          setStreamedText(finalText);
         }
       });
     }
 
-    return thinkingText;
+    return finalText;
   }
 
   const sendMessage = async (e) => {
     e.preventDefault();
 
-    if (!inputMessage.trim()) return;
+    const currentMessage = inputMessage.trim();
+    if (!currentMessage) return;
 
     // Add the sent message to the chat
-    setMessages([...messages, { text: inputMessage, type: "sent" }]);
-
-    const message = inputMessage
-    // Clear the input message
+    setMessages([...messages, { text: currentMessage, type: "sent" }]);
     setInputMessage('');
 
-    // Clear the textarea
-    if (textareaRef.current) textareaRef.current.value = '';
+    const finalText = await chat(currentMessage);
 
-    const receivedText = await chat(message);
-
-    // Clear the streamed text after receiving the final response
     setStreamedText('');
-
-    // Add the final response to the chat
-    setMessages((prevMessages) => [
+    setMessages(prevMessages => [
       ...prevMessages,
-      { text: receivedText, type: "received" }
+      { text: finalText, type: "received" }
     ]);
+
   };
 
-  const textareaRef = useRef(null);
-
   return (
-    <div className="container mx-auto py-16 px-4 max-h-screen overflow-y-auto mb-48 grid grid-cols-2 gap-8">
-      <div className="bg-gray-50 border-gray-100 p-6">
-        <div>
-          <span className="font-bold">Topic: </span>
-          <span>{requirements.topic}</span>
-        </div>
-        <div>
-          <span className="font-bold">Target Persona: </span>
-          <span>{requirements.target_persona}</span>
-        </div>
-        <div>
-          <span className="font-bold">Difficulty: </span>
-          <span>{requirements.difficulty_level}</span>
-        </div>
-
-        <div className="mt-8">
-          <h2 className="text-lg font-bold">Briefing</h2>
-          <div className="mt-4 prose">
-            <Markdown>{briefing}</Markdown>
-          </div>
-        </div>
-
-        <div>
-          <h2 className="text-lg font-bold mt-8">Questions</h2>
-          <div className="mt-4">
-            {questions && questions.map((question, index) => {
-              return (
-                <div key={index} className="mb-4 p-3 bg-white rounded-lg">
-                  <div className="">{question.title}</div>
-                  <ul className="list-disc text-sm pl-8">
-                    {question.options && question.options.map((option, optionIndex) => {
-                      return (
-                        <li key={optionIndex} className="">
-                          {option.value}
-                          {question.correct_option === optionIndex && (
-                            <span className="text-green-500 ml-2">(Correct)</span>
-                          )}
-                        </li>
-                      )
-                    })}
-                  </ul>
-                </div>
-              )
-            })}
-          </div>
-        </div>
+    <div className="max-w-5xl mx-auto py-16 px-4 max-h-screen overflow-y-auto mb-48">
+      <div className="flex justify-center text xs text-gray-500 mb-8">
+        <span className="mr-2">Thread ID:</span>
+        <input ref={threadId} type="text" defaultValue={Date.now().toString()} />
       </div>
-      <div className="">
-        {messages.map((message, index) => {
-          return message.type === "received" ? (
-            <div key={index} className="mt-8 prose">
-              <Markdown>{message.text}</Markdown>
-            </div>
-          ) : (
-            <div key={index} className="text-right">
-              <span className="inline-block bg-gray-100 px-6 py-3 rounded-full whitespace-pre-wrap">
-                {message.text}
-              </span>
-            </div>
-          )
-        })}
-        {streamedText !== '' && (
-          <div className="mt-8 prose">
-            <Markdown>{streamedText}</Markdown>
+      {messages.map((message, index) => {
+        return message.type === "received" ? (
+          <div key={index} className="mt-8 prose">
+            <Markdown>{message.text}</Markdown>
           </div>
-        )}
-      </div>
+        ) : (
+          <div key={index} className="text-right">
+            <span className="inline-block bg-gray-100 px-6 py-3 rounded-full whitespace-pre-wrap">
+              {message.text}
+            </span>
+          </div>
+        )
+      })}
+      {streamedText !== '' && (
+        <div className="mt-8 prose">
+          <Markdown>{streamedText}</Markdown>
+        </div>
+      )}
       <form
         onSubmit={sendMessage}
         className="max-w-4xl mx-auto p-4 flex gap-x-4 fixed bottom-0 left-0 right-0 mb-24">
